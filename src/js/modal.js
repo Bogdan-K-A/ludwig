ca6c85289143a1d3a8bc5eb21f094a1ce06de534
Bogdan-K-A <kozhoma_b@ukr.net>
2023-02-19 11:16:31 +0200

    create pause video and add style

--- src/js/modal.js
+++ src/js/modal.js
@@ -11,15 +11,7 @@
 
 	function toggleModal() {
 		refs.modal.classList.toggle("is-hidden");
-		const src = refs.video.hasAttribute("src");
-
-		if (src === false) {
-			refs.video.setAttribute(
-				"src",
-				`https://video.wixstatic.com/video/9aa138_e2bcbffd9deb4c9f9bca4e82a1cbbc99/1080p/mp4/file.mp4`
-			);
-		} else {
-			refs.video.removeAttribute("src");
-		}
+		refs.video.pause();
+		refs.video.currentTime = 0;
 	}
 })();
